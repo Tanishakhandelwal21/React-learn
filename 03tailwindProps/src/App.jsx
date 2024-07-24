34493730b0708761971b55a52a1d882b1fb2e6d4
Tanishakhandelwal21 <tanishakhandelwal.5hy@gmail.com>
2024-07-24 09:47:59 +0530

    props and tailwind complete

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -6,11 +6,22 @@ import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
+  let myObj = {
+    username: "Tanisha",
+    age: 21,
+  };
+  let myArr = [1, 2, 3];
 
   return (
     <>
       <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind test</h1>
-      <Card/>
+      <Card
+        userName="tanisha"
+        btnTxt="visit me!"
+        detail="Software developer"
+        obj={myArr}
+      />
+      <Card userName="vishu" btnTxt="click me!" detail="Data Scientist" />
     </>
   );
 }
